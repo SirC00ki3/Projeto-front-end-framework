a966738de689f2982e1cbbf257e555ed2e89a30a
SirC00ki3 <caioraul36@gmail.com>
2025-09-30 12:54:50 -0300

    busca do home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './Header.css';
+import logo from "../../imagens/icons/logo.png";
 
 function Header() {
   return (
     <header className="header">
-      <h1>Concha</h1>
-      <nav className="header__nav">
-        <ul>
-          <li><a href="#home">Início</a></li>
-          <li><a href="#series">Séries</a></li>
-          <li><a href="#movies">Filmes</a></li>
-          <li><a href="#new">Novidades</a></li>
-        </ul>
-      </nav>
+      <img src={logo} alt="StreamFlix Logo" className="header__logo" />
+
+      <div className="header__nav-wrapper"> {/* <-- Novo div para agrupar os elementos */}
+        <nav className="header__nav">
+          <ul>
+            <li><a href="#home">Início</a></li>
+            <li><a href="#series">Séries</a></li>
+            <li><a href="#movies">Filmes</a></li>
+            <li><a href="#new">Novidades</a></li>
+          </ul>
+        </nav>
+
+        <div className="header__search">
+          <form>
+            <input type="text" placeholder="Buscar filmes..." />
+            <button type="submit">Buscar</button>
+          </form>
+        </div>
+      </div>
     </header>
   );
 }
